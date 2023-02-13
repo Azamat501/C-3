5d12966f68acda3e9b86821984dd8bb3196c81bb
azamat yusupov <ayi-azamat1990@mail.ru>
2023-02-13 19:19:28 +0500

    new code

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a;
+Console.WriteLine("Введите числ:");
+a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else 
+{
+    Console.WriteLine("нет ");
+}
+
+Console.ReadKey();
+
+
